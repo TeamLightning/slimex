5ffaf5f5f0e85c7c816d9f481e57eed5d9a76435
xXAlphaManXx <smartclashcoc@gmail.com>
2017-05-15 11:21:54 +0530

    Integrate travis and some changes

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,3 +1,15 @@
 <?php
 
+/**
+ * --------------------------------------------------
+ *  The main router
+ * --------------------------------------------------
+ *
+ * You can place all your routes here. You can place
+ * groups, map requests, or slim specific GET, POST,
+ * HEAD, PATCH, DELETE requests too.
+ *
+ * @see https://www.slimframework.com/docs/objects/router.html
+ */
+
 $app->get('/', 'HomeController:index');
